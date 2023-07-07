fb79321743c26e59d68eb7f20dce13b652f2e6f2
LeonardoCZ <PC@LeonardoCZ>
2023-07-06 20:00:46 -0600

    Sexto commit del proyecto Tineda Commit de los productos con el crud

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -4,42 +4,43 @@
  */
 package com.tienda.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 
 /**
  *
  * @author PC
  */
-
 @Data
 @Entity
-@Table(name="categoria")
-public class Categoria implements Serializable {
+@Table(name="producto")
+public class Producto implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
-    private Long idCategoria;
+    @Column(name="id_producto")
+    private Long idProducto;
     private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
     private String rutaImagen;
     private boolean activo;
     
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name="id_categoria")
-    List<Producto> productos;
+    Categoria categoria;
     
-    public Categoria(){
-        
+    public Producto(){      
     }
-    
-    public Categoria(String categoria, boolean activo){
-        this.descripcion = categoria;
-        this.activo = activo;
-    }
-    
 }
